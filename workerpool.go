08adbed48a5d960a8fac4533e705b5c3ff58a6e3
freepk <freepk@mail.ru>
2018-05-28 15:31:11 +0300

    refactor

--- workerpool.go
+++ workerpool.go
@@ -1,8 +1,6 @@
 package workerpool
 
-type Task interface {
-	Run()
-}
+type Task func()
 
 type worker struct {
 	queue chan chan Task
@@ -14,7 +12,7 @@ func (w *worker) start() {
 		w.queue <- w.task
 		select {
 		case task := <-w.task:
-			task.Run()
+			task()
 		}
 	}
 }
--- workerpool_test.go
+++ workerpool_test.go
@@ -1,41 +1,14 @@
 package workerpool
 
 import (
-	"sync/atomic"
 	"testing"
 )
-
-var (
-	testCounter = int64(0)
-)
-
-type task0 struct {
-	name string
-}
-
-func (t task0) Run() {
-	atomic.AddInt64(&testCounter, 1)
-}
-
-type task1 struct {
-	name string
-}
-
-func (t task1) Run() {
-	atomic.AddInt64(&testCounter, 1)
-}
-
-type task2 int
-
-func (t task2) Run() {
-	atomic.AddInt64(&testCounter, 1)
-}
 
 func TestWorkerPool128(b *testing.T) {
 	p := NewPool(128)
 	go p.Start()
-	x := &task0{name: "task0"}
-	y := &task1{name: "task1"}
+	x := func() {}
+	y := func() {}
 	for i := 0; i < 1000000; i++ {
 		p.Run(x)
 		p.Run(y)
@@ -45,7 +18,7 @@ func TestWorkerPool128(b *testing.T) {
 func BenchmarkPool(b *testing.B) {
 	p := NewPool(128)
 	go p.Start()
-	var t task2
+	t := func() {}
 	for i := 0; i < b.N; i++ {
 		p.Run(t)
 	}
